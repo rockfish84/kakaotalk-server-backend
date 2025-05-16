807be206504f8db82d7c0385c2bdb0fc942326dd
luffy84 <kjhdinomath@gmail.com>
2025-05-16 22:17:33 +0900

    message

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const admin = require('firebase-admin');
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    projectId: serviceAccount.project_id
 });
 
 const app = express();
